f4b2ca3089d86ea650682e66dc0b366c3207b95a
Fredy Blandon <fsblandon@gmail.com>
2019-06-11 00:14:52 -0500

    modify api

--- src/api/books.js
+++ src/api/books.js
@@ -11,12 +11,8 @@ const massage = (book) => ({
 const URL = 'http://www.etnassoft.com/';
 
 export const getBooks = () =>
-  axios.get(URL + `api/v1/get/?category=libros_programacion`, 
-  {
-    headers: {
-        'Access-Control-Allow-Origin': true,
-    }
-  })
+    axios.defaults.headers = 'Access-Control-Allow-Origin';
+    axios.get(URL + `api/v1/get/?category=libros_programacion`)
     .then((response) => {
         console.log(response);
         response.data.results.map((book) => massage(book))
